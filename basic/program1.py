08ffa39b98be82d7d67b26045b95560dc9fdd2b3
Sumama Rahim <sumama.rahim@yandex.com>
2025-01-02 14:21:46 -0500

    Add files via upload

---
+++ basic/program1.py
@@ -0,0 +1,17 @@
+print ("*** Trying Something New ***")
+
+def new_way():
+	num1 = 3
+	num2 = 6
+	print ("The sum of ", num1 ," and ", num2 , " = ", num1+num2)
+
+# calling the function
+new_way()
+
+# concatination
+string1 = "String1 is Here"
+string2 = "String2 is here"
+string3 = string1+" "+ string2 # space is also added
+print (string3)
+
+
